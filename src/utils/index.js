4cda1ff24f51d3603145236f55895407627b39bd
hyunjin <alfma123@naver.com>
2023-09-21 23:59:10 +0900

    feat: generateToken 함수 생성

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,12 @@
 const { default: Axios } = require('axios');
+const jwt = require('jsonwebtoken');
 
 exports.axios = Axios.create({
   headers: {
     'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
   },
 });
+
+exports.generateToken = (id) => {
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
+};
