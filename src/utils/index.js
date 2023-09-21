62b6ab313f5beeda242cdae0dab33c1b984e5a9f
hyunjin <alfma123@naver.com>
2023-09-22 00:00:13 +0900

    feat: throwError 함수 생성

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,3 +10,19 @@ exports.axios = Axios.create({
 exports.generateToken = (id) => {
   jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
+
+exports.throwError = (code, message) => {
+  if (!code) return;
+  const error = new Error();
+  const errorMessage = new Map([
+    [400, 'bad request'],
+    [401, 'unAuthorized'],
+    [500, 'internal server error'],
+  ]);
+  if (!errorMessage.get(code) || message) {
+    errorMessage.set(code, message);
+  }
+  error.message = errorMessage.get(code);
+  error.status = code;
+  throw error;
+};
