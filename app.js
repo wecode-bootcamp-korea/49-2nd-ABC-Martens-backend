80af224ce777b0546347fb591482bf12c05d3ab2
신유진 <julia011097@gmail.com>
2023-09-22 18:27:26 +0900

    로그인 일단 api 코드 전체 완료(기본적 토큰 담기)

--- app.js
+++ app.js
@@ -29,80 +29,74 @@ app.listen(app.get('port'), () => {
   console.log(`listening.... 🦻http://localhost:${app.get('port')}`);
 });
 
-
 //로그인
-app.post("/login", async (req, res) => {
+app.post('/login', async (req, res) => {
   try {
     const email = req.body.email;
     const password = req.body.password;
 
-    // key error 
-    if ( !email || ! password ) {
-      const error = new Error("필수 입력란을 모두 작성해 주세요");
+    // key error
+    if (!email || !password) {
+      const error = new Error('필수 입력란을 모두 작성해 주세요');
       error.statusCode = 400;
       throw error;
     }
     // key error 할 수 있지만, 프론트엔드에게 백엔드의 의도를 '친절하게' 알려주기 위해
 
-
     // email 이 DB에 있는지 (existing user인지)
     const existingUser = await myDataSource.query(`
     SELECT id, email, password FROM users WHERE email='${email}';
     `);
-    console.log("existing user:", existingUser);
+    console.log('existing user:', existingUser);
 
     if (existingUser.length === 0) {
-      const error = new Error("일치하는 회원정보가 없습니다");
+      const error = new Error('일치하는 회원정보가 없습니다');
       error.statusCode = 400;
       throw error;
     }
 
     // 해당 email의 password가 DB에 있는지 (existing user인지)
     if (password !== existingUser[0].password) {
-      const error = new Error("일치하는 회원 정보가 없습니다");
+      const error = new Error('일치하는 회원 정보가 없습니다');
       error.statusCode = 400;
       throw error;
     }
-     보안을 위해 비밀번호, 패스워드 중 오류 알려주지 않기로
+    //  보안을 위해 비밀번호, 패스워드 중 오류 알려주지 않기로
 
-    if (password !== existingUser[0].password) {
-    const error = new Error("INVALID_PASSWORD");
-    error.statusCode = 400;
-    throw error;
-    // }
-
+    // 해당 email의 해쉬된 패스워드가 DB에 있는가
     const hashPw = await bcrypt.compare(password, existingUser[0].password);
-    console.log(hashPw)
-    
-        if (!hashPw) {
-          const error = new Error("passwordError");
-          error.statusCode = 400;
-          error.code = "passwordError";
-          throw error;
-        }
+    console.log(hashPw);
 
+
+    if (!hashPw) {
+      const error = new Error('패스워드가 일치하지 않습니다');
+      error.statusCode = 400;
+      error.code = 'passwordError';
+      throw error;
+    }
+
+    // 로그인 성공 시 토큰 발급 
+    const token = jwt.sign({ id: existingUser[0].id }, process.env.TYPEORM_JWT);
     return res.status(200).json({
-      message: "로그인 성공하였습니다",
+      message: '로그인 성공하였습니다',
       accessToken: token,
     });
   } catch (error) {
     console.log(error);
   }
-});
+});v 
 
 const server = http.createServer(app);
 const start = async () => {
   try {
-    server.listen(7801, () => console.log(`Server is listening on 7801`));
+    server.listen(8000, () => console.log(`Server is listening on 8000`));
   } catch (err) {
     console.error(err);
   }
 };
 
 myDataSource.initialize().then(() => {
-  console.log("Data Source has been initialized!");
+  console.log('Data Source has been initialized!');
 });
 
 start();
-
-
