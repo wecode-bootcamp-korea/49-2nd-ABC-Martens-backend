7ad8696f9366aec62aa6c7b9aa6f044420b0f25d
Seungwon Kim <kim.sw94@gmail.com>
2023-10-02 16:32:10 +0900

    fix: 불필요한 로그를 지웠습니다.

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,10 +2,7 @@ const express = require('express');
 const productController = require('../controllers/productController');
 const router = express.Router();
 const detail = productController.detail;
-const deleter = productController.deleter;
-const creator = productController.creator;
 
-//상세페이지 관련 기능입니다.
 router.get('/detail/:id', productController.detail);
 
 module.exports = router;
