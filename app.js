992c5d6f8a341054bbcadf1291bd130e357d29e1
신유진 <julia011097@gmail.com>
2023-10-04 16:57:42 +0900

    insert into , 추가 / 마케팅 수신 삭d git commit -m dd git add .

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const { dataSource: myDataSource } = require('./src/models/dataSource');
 
 require('dotenv').config();
 
