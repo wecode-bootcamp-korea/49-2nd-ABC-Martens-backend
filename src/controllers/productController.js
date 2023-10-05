e37bd189b29347eb5fc5705828182a52552a87fd
Seungwon Kim <kim.sw94@gmail.com>
2023-10-05 14:28:56 +0900

    fix: 컨트롤러에서 처리하던 응답형식 수정을 서비스 레이어로 이동했습니다.

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,31 +5,7 @@ const detail = async (req, res) => {
   try {
     const { id } = req.params;
     const productReader = await productService.productReader(Number(id));
-
-    const extractProductDetails = (productReader) => {
-      const productId = productReader.productIntroducer[0].id;
-      const productName = productReader.productIntroducer[0].product_name;
-      const price = productReader.productIntroducer[0].price;
-      const originalPrice = productReader.productIntroducer[0].original_price;
-      const detailImageUrl = productReader.imageSelector[0].detail_image_url;
-      const thumbnailImageUrl =
-        productReader.imageSelector[0].thumbnail_image_url;
-      const isThumbnail = productReader.imageSelector[0].is_thumbnail;
-      const option = productReader.options;
-
-      return {
-        productId,
-        productName,
-        price,
-        originalPrice,
-        detailImageUrl,
-        thumbnailImageUrl,
-        isThumbnail,
-        option,
-      };
-    };
-
-    return res.status(200).json({ data: extractProductDetails(productReader) });
+    return res.status(200).json({"data" : productReader});
   } catch (err) {
     console.error(err);
     next(err);
--- src/services/productService.js
+++ src/services/productService.js
@@ -5,16 +5,28 @@ const { selector, introducer, imageLoader, option, price } = productDao;
 const productReader = async (id) => {
   try {
     const productIntroducer = await productDao.introducer(id);
+      const productId = productIntroducer[0].id;
+      const productName = productIntroducer[0].product_name;
+      const price = productIntroducer[0].price;
+      const orignialPrice = productIntroducer[0].original_price;
     const imageSelector = await productDao.imageLoader(id);
+      const detailImageUrl = imageSelector[0].detail_image_url;
+      const thumbnailImageUrl =imageSelector[0].thumbnail_image_url;
+      const isThumbnail = imageSelector[0].is_thumb_nail
     const optionSelector = await productDao.option(id);
 
-    const responseReader = {
-      productIntroducer,
-      imageSelector,
+    const data = {
+      productId,
+      productName,
+      price,
+      orignialPrice,
+      detailImageUrl,
+      thumbnailImageUrl,
+      isThumbnail,
       options: optionSelector,
     };
 
-    return responseReader;
+    return data;
 
   } catch (err) {
     console.error(err);
