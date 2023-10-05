69a341bf45c82dfec6d6f735cc6bb1d63a5670ff
신유진 <julia011097@gmail.com>
2023-10-05 18:27:40 +0900

    localhost 삭제

--- app.js
+++ app.js
@@ -184,7 +184,6 @@ app.use((err, _, res, next) => {
 
 app.listen(
   app.get('port'),
-  process.env.NODE === 'production' ? '0.0.0.0' : 'localhost',
   () => {
     console.log(`server is running`);
   },
